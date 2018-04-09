722320367030d9e46d86cb216b596ced0778a0a6
sergiopoliveira <sergiopoliveira@outlook.com>
2018-04-09 16:53:45 +0100

    Hibernate CRUD using Annotations

---
+++ src/com/sergio/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,46 @@
+package com.sergio.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.sergio.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Student.class)
+								 .buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+
+		// use the session object to save Java object
+		try {
+			
+			// create a student object
+			System.out.println("Creating a new student object...");
+			Student tempStudent = new Student("Sergio", "Oliveira", "sergio@sergio.com");
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the student object
+			System.out.println("Saving the student...");
+			session.save(tempStudent);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
